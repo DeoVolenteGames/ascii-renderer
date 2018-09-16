2d136734e72025c43396c2869f05d53640aefe86
Rory <rory@deovolente.games>
2018-09-16 21:06:05 +1200

    Split ascii-renderer and boilerplate code completely
    Now it should work with whatever `THREE.Renderer` you want to throw at it.
    
    However, it shows how much work needs to be done to make it accurate to any canvas size and font size.
    
    Also updated name in package.json.

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 ## TODO: list
 
 1. Toon lighting has issues when viewed from behind. Try a custom lighting model.
-1. Separate ascii-renderer from THREE setup code even further.
-1. Fix font rendering.
+1. Fix font size rendering so that any font sizes and canvas size can be used.
+1. Add font ratio detection so that any monospace font can be used.
+1. Set up [pages](https://pages.github.com/#vanilla-step-1).
+1. Add minification.
--- public/ascii-renderer.js
+++ public/ascii-renderer.js
@@ -1,88 +1,66 @@
-AsciiRenderer = function(element, texture, options) {
+/*
+ * @author DeoVolenteGames | https://github.com/DeoVolenteGames
+ *
+ * Creates an SVG element as a mask and correctly sizes canvas to render behind it.
+ *
+ */
+
+AsciiRenderer = function(renderer, options) {
 
   if ( ! options ) options = {};
   var charSet = !options[ 'charSet' ] ? '01' : options[ 'charSet' ];
-  // TODO: Support font sizes other than 12
   var fontSize = !options[ 'fontSize' ] ? 12 : options[ 'fontSize' ];
-  var useControls = !options[ 'stats' ] ? false : options[ 'controls' ];
-  var useOrthoCam = !options[ 'ortho' ] ? false : options[ 'ortho' ];
-  var useStats = !options[ 'stats' ] ? false : options[ 'stats' ];
 
-  var container, stats, canvas;
+  var link = document.createElement('link');
+  link.setAttribute('rel', 'stylesheet');
+  link.setAttribute('type', 'text/css');
+  link.setAttribute('href', 'https://fonts.googleapis.com/css?family=Inconsolata&text=01');
+  document.head.appendChild(link);
 
-  var camera, controls, scene, renderer;
+  var style=document.createElement('style');
+  style.type='text/css';
+  var styleStr = `
+  canvas.ascii-canvas {
+    image-rendering: -moz-crisp-edges;
+    image-rendering: -webkit-crisp-edges;
+    image-rendering: pixelated;
+    image-rendering: crisp-edges;
+  }`;
+  if(style.styleSheet){
+      style.styleSheet.cssText=styleStr;
+  }else{
+      style.appendChild(document.createTextNode(styleStr));
+  }
+  document.head.appendChild(style);
 
-  var sphere;
+  var canvas = renderer.domElement;
+  canvas.classList += 'ascii-canvas';
+  if (canvas.parentNode === null) {
+    console.error('THREE.Renderer\'s canvas.parentNode is null. Make to sure' +
+                 ' to attach the renderer to a container before initialising' +
+                 ' the ascii-renderer.');
+    return;
+  }
 
-  var start = Date.now();
+  var svg = document.createElementNS ('http://www.w3.org/2000/svg', 'svg');
+  svg.style.width = '100%';
+  svg.style.height = '100%';
+  svg.style.fontSize = fontSize + 'px';
+  svg.style.fontFamily = '"Inconsolata", monospace';
+  svg.style.position = 'absolute';
+  canvas.parentNode.appendChild(svg);
+  this.svg = svg;
 
-  init();
-  animate();
+  this.setSize = function(width, height) {
 
-  function init() {
-    var link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('href', 'https://fonts.googleapis.com/css?family=Inconsolata&text=01');
-    document.head.appendChild(link);
+    var iWidth = Math.round( width / fontSize / 0.5 );
+    var iHeight = Math.round( height / fontSize );
 
-    container = document.createElement( 'div' );
-    container.style.height = '100%';
-    container.style.width = '100%';
-    container.style.position = 'relative';
-    container.style.fontFamily = '"Inconsolata", monospace';
-    element.appendChild( container );
-
-    canvas = document.createElement('canvas');
-    canvas.classList += 'ascii-canvas';
-    var style=document.createElement('style');
-    style.type='text/css';
-    var styleStr = `
-    canvas.ascii-canvas {
-      image-rendering: -moz-crisp-edges;
-      image-rendering: -webkit-crisp-edges;
-      image-rendering: pixelated;
-      image-rendering: crisp-edges;
-    }`;
-    if(style.styleSheet){
-        style.styleSheet.cssText=styleStr;
-    }else{
-        style.appendChild(document.createTextNode(styleStr));
-    }
-    document.head.appendChild(style);
-    container.appendChild( canvas );
-
-    renderer = new THREE.WebGLRenderer( { canvas: canvas, alpha: true } );
-
-    var width = container.clientWidth || 2;
-    var height = container.clientWidth || 2;
-
-    if (useOrthoCam) {
-      camera = new THREE.OrthographicCamera( width / -2, width / 2, height / 2, height / -2, 1, 1000 );
-    }
-    else {
-      camera = new THREE.PerspectiveCamera( 25, width / height, 1, 1000 );
-    }
-    camera.position.z = 1000;
-
-    scene = new THREE.Scene();
-
-    var sunlight = new THREE.DirectionalLight( 0xffffff, 1.5 );
-    sunlight.position.set( 1, 0, 1 );
-    scene.add( sunlight );
-
-    var sphereTex = new THREE.TextureLoader().load(texture);
-    sphereTex.magFilter = THREE.NearestFilter;
-    sphereTex.minFilter = THREE.NearestFilter;
-    var sphereMat = new THREE.MeshToonMaterial({
-      map: sphereTex,
-      color: 0xffffff,
-      shininess: 0,
-      reflectivity: 0,
-    });
-
-    sphere = new THREE.Mesh( new THREE.SphereBufferGeometry( 200, 20, 10 ), sphereMat );
-    scene.add( sphere );
+    // TODO: calculate remainder of width to resize by
+    renderer.setSize( iWidth, iHeight );
+    canvas.style.width = width - 1 + 'px';
+    canvas.style.height = height + 'px';
+    canvas.style.position = 'absolute';
 
     var i = 0;
     var strArray = [];
@@ -98,12 +76,7 @@ AsciiRenderer = function(element, texture, options) {
     }
     strArray.push('</tspan>');
 
-    var svg = document.createElementNS ('http://www.w3.org/2000/svg', 'svg');
-    svg.style.width = '100%';
-    svg.style.height = '100%';
-    svg.style.fontSize = fontSize + 'px';
-    svg.style.position = 'absolute';
-    svg.innerHTML += `
+    svg.innerHTML = `
       <defs>
         <mask id="ascii-mask" x="0" y="0" width="100%" height="100%" >
           <rect fill="white" x="0" y="0" width="100%" height="100%"></rect>
@@ -112,63 +85,9 @@ AsciiRenderer = function(element, texture, options) {
           `</text>
         </mask>
       </defs>
-      <rect style="mask:url(#ascii-mask);fill:var(--ascii-bg-color,#000);opacity:0.7;" x="0" y="0" width="100%" height="100%"/></rect>`;
-    container.appendChild(svg);
-
-    // TODO: initialise controls seperately
-    if (useControls) {
-      controls = new THREE.OrbitControls( camera, svg );
-      controls.enablePan = false;
-      controls.enableZoom = false;
-      // controls.minPolarAngle = Math.PI/2;
-      // controls.maxPolarAngle = Math.PI/2;
-      controls.update();
-    }
-
-    // TODO: initialise controls seperately
-    if (useStats) {
-      stats = new Stats();
-      stats.showPanel( 0 );
-      container.appendChild( stats.domElement );
-    }
-
-    window.addEventListener( 'resize', onWindowResize, false );
-    onWindowResize();
+      <rect style="mask:url(#ascii-mask);fill:var(--ascii-bg-color,#000);opacity:0.7;"
+            x="0" y="0" width="100%" height="100%"/></rect>`;
   }
 
-  function onWindowResize() {
-    var width = container.clientWidth;
-    var height = container.clientHeight;
-
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    var iWidth = Math.round( width / fontSize / 0.5 );
-    var iHeight = Math.round( height / fontSize );
-
-    // TODO: calculate remainder of width to resize by
-    renderer.setSize( iWidth, iHeight );
-    canvas.style.width = width - 1 + 'px';
-    canvas.style.height = height + 'px';
-    canvas.style.position = 'absolute';
-  }
-
-  //
-
-  function animate() {
-    requestAnimationFrame( animate );
-    if (useStats)
-      stats.begin();
-
-    render();
-
-    if (useStats)
-      stats.end();
-  }
-
-  function render() {
-    var timer = Date.now() - start;
-    sphere.rotation.y = timer * 0.0002;
-    renderer.render( scene, camera );
-  }
+  // this.setSize(canvas.width, canvas.height);
 }
